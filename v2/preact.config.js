f30cf1410cbcafbf994ff23dc7a1b14609347db0
Artjom Kurapov <artkurapov@gmail.com>
2023-01-04 04:02:55 +0200

    migrating to typescript build

---
+++ v2/preact.config.js
@@ -0,0 +1,69 @@
+import preactCliSvgLoader from 'preact-cli-svg-loader'
+import path from 'path'
+import WebpackPwaManifest from 'webpack-pwa-manifest'
+// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
+
+/**
+ * Function that mutates the original webpack config.
+ * Supports asynchronous changes when a promise is returned (or it's an async function).
+ *
+ * @param {import('preact-cli').Config} config - original webpack config
+ * @param {import('preact-cli').Env} env - current environment and options pass to the CLI
+ * @param {import('preact-cli').Helpers} helpers - object with useful helpers for working with the webpack config
+ * @param {Record<string, unknown>} options - this is mainly relevant for plugins (will always be empty in the config), default to an empty object
+ */
+export default (config, env, helpers, options) => {
+	// helpers.webpack.plugins = [new BundleAnalyzerPlugin()]
+
+	config.module.rules.push(
+		  {
+			// For pure CSS - /\.css$/i,
+			// For Sass/SCSS - /\.((c|sa|sc)ss)$/i,
+			// For Less - /\.((c|le)ss)$/i,
+			test: /\.less$/i,
+			use: [
+			//   "style-loader",
+			//   {
+			// 	loader: "css-loader",
+			// 	options: {
+			// 	  // Run `postcss-loader` on each CSS `@import`, do not forget that `sass-loader` compile non CSS `@import`'s into a single file
+			// 	  // If you need run `sass-loader` and `postcss-loader` on each CSS `@import` please set it to `2`
+			// 	  importLoaders: 1,
+			// 	},
+			//   },
+			//   {
+			// 	loader: "postcss-loader",
+			// 	options: { plugins: () => [postcssPresetEnv({ stage: 0 })] },
+			//   },
+			//   // Can be `less-loader`
+			  {
+				loader: "less-loader",
+			  },
+			],
+		  });
+
+	helpers.webpack.optimization = {
+		splitChunks: {
+			chunks: 'all',
+		},
+	}
+
+	config.plugins.push(
+		new WebpackPwaManifest({
+			name: 'Gratheon',
+			filename: 'manifest.json',
+			ios: true,
+			fingerprints: false,
+			short_name: 'Gratheon',
+			description: 'Beehive management',
+			background_color: '#ffffff',
+			icons: [
+				{
+					src: path.resolve('src/assets/favicons/android-chrome-512x512.png'),
+					sizes: [96, 128, 192, 256, 384, 512],
+				},
+			],
+		})
+	)
+	preactCliSvgLoader(config, helpers)
+}
---
+++ v2/src/components/page/hiveEdit/boxes/index.tsx
@@ -0,0 +1,212 @@
+import find from 'lodash/find'
+import filter from 'lodash/filter'
+import isNil from 'lodash/isNil'
+
+import Box from './box'
+import { isFrameWithSides } from '../../../api/storage/frames'
+
+import SelectedFrame from './selectedFrame'
+import FrameButtons from './box/frameButtons'
+import Button from '../../../shared/button'
+import styles from './styles.less'
+import React from 'preact/compat'
+import { boxTypes } from '../../../api/storage/boxes'
+import AddBoxIcon from '../../../../icons/addBox'
+// import ListInspections from '../../../shared/listInspections'
+
+type BoxesProps = {
+	hiveId: any
+	frames: any
+	boxes: any
+	apiaryId: any
+	boxSelected: any
+	frameSelected: any
+	frameSide: any
+
+	editable?: boolean
+
+	onBoxClick?: any
+	onBoxRemove?: any
+	onBoxAdd?: any
+	onMoveDown?: any
+
+	onFrameClose?: any
+	onFrameAdd?: any
+	onFrameSideFileUpload?: any
+	onDragDropFrame?: any
+	onFrameSideStatChange?: any
+	onFrameRemove?: any
+}
+
+export default ({
+	hiveId,
+	frames,
+	boxes,
+	apiaryId,
+	boxSelected,
+	frameSelected = 0,
+	frameSide,
+
+	editable = true,
+
+	onBoxClick = () => {},
+	onBoxRemove = () => {},
+	onBoxAdd = () => {},
+	onMoveDown = () => {},
+
+	onFrameClose = () => {},
+	onFrameAdd = () => {},
+	onFrameSideFileUpload = () => {},
+	onDragDropFrame = () => {},
+	onFrameSideStatChange = () => {},
+	onFrameRemove = () => {},
+}: BoxesProps) => {
+	const boxesDivs = []
+	let selectedFrameSide = null
+
+	for (
+		let boxDivPosition = 0;
+		boxDivPosition < boxes.length;
+		boxDivPosition++
+	) {
+		const box = boxes[boxDivPosition]
+		const currentBoxSelected = !editable || box.position === boxSelected
+		const showDownButton = boxes.length - 1 !== boxDivPosition
+		const boxFrames = filter(frames, { boxIndex: box.position })
+
+		if (editable && !isNil(frameSelected) && !isNil(boxSelected)) {
+			const selectedFrame = find(frames, {
+				position: frameSelected,
+				boxIndex: box.position,
+			})
+			const frameWithSides =
+				!isNil(frameSide) &&
+				selectedFrame &&
+				isFrameWithSides(selectedFrame.type)
+			const frameSideObject = frameWithSides
+				? frameSide === 'left'
+					? selectedFrame.leftSide
+					: selectedFrame.rightSide
+				: null
+
+			if (currentBoxSelected && selectedFrame) {
+				const onUpload = (uploadedFile: any) => {
+					onFrameSideFileUpload({
+						boxIndex: box.position,
+						position: selectedFrame.position,
+						side: frameSide,
+						uploadedFile,
+					})
+				}
+
+				selectedFrameSide = (
+					<SelectedFrame
+						boxSelected={boxSelected}
+						frameSelected={frameSelected}
+						box={box}
+						hiveId={hiveId}
+						selectedFrame={selectedFrame}
+						frameWithSides={frameWithSides}
+						frameSide={frameSide}
+						frameSideObject={frameSideObject}
+						onUpload={onUpload}
+						onFrameClose={onFrameClose}
+						onFrameSideStatChange={onFrameSideStatChange}
+					/>
+				)
+			}
+		}
+
+		boxesDivs.push(
+			<div
+				style={{marginBottom:15}}
+				onClick={(event) => {
+					onBoxClick({ event, boxIndex: box.position })
+				}}
+			>
+				{editable && currentBoxSelected && (
+					<div style={{height: 35}}>
+						<FrameButtons
+							frameSelected={frameSelected}
+							onFrameRemove={onFrameRemove}
+							onMoveDown={onMoveDown}
+							onBoxRemove={onBoxRemove}
+							onFrameAdd={onFrameAdd}
+							showDownButton={showDownButton}
+							box={box}
+						/>
+					</div>
+				)}
+
+				<div className={styles.box}>
+					<Box
+						boxType={box.type}
+						boxPosition={box.position}
+						boxSelected={boxSelected}
+						editable={editable}
+						frameSelected={frameSelected}
+						frameSide={frameSide}
+						frames={boxFrames}
+						hiveId={hiveId}
+						apiaryId={apiaryId}
+						onDragDropFrame={(args: any) => {
+							const { removedIndex, addedIndex, event } = args
+
+							onDragDropFrame({
+								event,
+								onDragDropFrame,
+								removedIndex,
+								addedIndex,
+								hiveId,
+								frameSide,
+								boxIndex: boxSelected,
+							})
+						}}
+						// onFrameAdd={() => onFrameAdd(box.position)}
+					/>
+				</div>
+			</div>
+		)
+	}
+
+	return (
+		<div style={{display:'flex', padding:'0 20px'}}>
+			<div style={{paddingRight: 5, overflow:'hidden',flexGrow:3}}>
+				<div style={{display:'flex',height:40}}>
+					<h3 style={{flexGrow:1}}>Hive sections</h3>
+
+					<div style={{display:'flex'}}>
+						<Button
+							title="Add box on top"
+							className={['small', 'black']}
+							onClick={() => onBoxAdd(boxTypes.DEEP)}
+						>
+							<AddBoxIcon /> Add deep
+						</Button>
+						<Button
+							title="Add box on top"
+							onClick={() => onBoxAdd(boxTypes.SUPER)}
+						>
+							<AddBoxIcon /> Add super
+						</Button>
+					</div>
+				</div>
+
+				<div>{boxesDivs}</div>
+			</div>
+
+			{selectedFrameSide}
+
+			{/* Notes */}
+			{/* {!selectedFrameSide && (
+				<div style="flex-grow:6">
+					<ListInspections
+						apiaryId={apiaryId}
+						inspections={hive.inspections}
+						hive={hive}
+					/>
+				</div>
+			)} */}
+		</div>
+	)
+}

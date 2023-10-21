d5e78eedc718257cfe09bf76543a5517cd1049d8
Artjom Kurapov <artjom.kurapov@clarifai.com>
2023-10-21 17:18:17 +0300

    don't track metrics in dev

--- src/components/metrics.tsx
+++ src/components/metrics.tsx
@@ -1,16 +1,35 @@
 import * as amplitude from '@amplitude/analytics-browser';
+import isDev from './isDev';
 amplitude.init('22c65699d4c0d9ee32ba08a9c3087dcd', {
 	defaultTracking: true,
-  }); // API key, should be safe to be public
+}); // API key, should be safe to be public
 
 export default {
 	setUserId: amplitude.setUserId,
 	track: amplitude.track,
 
-	trackLogin: (extraInfo={})=>{ amplitude.track('user.logged_in', extraInfo)},
-	trackRegistration: (extraInfo={})=>{ amplitude.track('user.registered', extraInfo)},
-	trackApiaryCreated: (extraInfo={})=>{ amplitude.track('apiary.created', extraInfo)},
-	trackBoxCreated: (extraInfo={})=>{ amplitude.track('box.created', extraInfo)},
-	trackFrameAdded: (extraInfo={})=>{ amplitude.track('frame.created', extraInfo)},
-	trackFramePhotoUploaded: (extraInfo={})=>{ amplitude.track('frame.photoUploaded', extraInfo)}
+	trackLogin: (extraInfo = {}) => {
+		if (isDev()) return
+		amplitude.track('user.logged_in', extraInfo)
+	},
+	trackRegistration: (extraInfo = {}) => {
+		if (isDev()) return
+		amplitude.track('user.registered', extraInfo)
+	},
+	trackApiaryCreated: (extraInfo = {}) => {
+		if (isDev()) return
+		amplitude.track('apiary.created', extraInfo)
+	},
+	trackBoxCreated: (extraInfo = {}) => {
+		if (isDev()) return
+		amplitude.track('box.created', extraInfo)
+	},
+	trackFrameAdded: (extraInfo = {}) => {
+		if (isDev()) return
+		amplitude.track('frame.created', extraInfo)
+	},
+	trackFramePhotoUploaded: (extraInfo = {}) => {
+		if (isDev()) return
+		amplitude.track('frame.photoUploaded', extraInfo)
+	}
 }

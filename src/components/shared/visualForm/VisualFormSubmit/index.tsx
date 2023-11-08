6e35045dafb7577f4641a68286285712ad3b032b
Artjom Kurapov <artjom.kurapov@clarifai.com>
2023-11-09 01:06:15 +0200

    update apiary edit view

--- src/components/shared/visualForm/VisualFormSubmit/index.tsx
+++ src/components/shared/visualForm/VisualFormSubmit/index.tsx
@@ -4,7 +4,7 @@ export default ({ children }) => {
 	return (
 		<div>
 			<div></div>
-			<div style="text-align:right;">
+			<div style="text-align:right;display:flex;flex-direction: row-reverse;">
 				{children}
 			</div>
 		</div>

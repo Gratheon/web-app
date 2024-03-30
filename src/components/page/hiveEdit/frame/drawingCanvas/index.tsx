6f18ce3bc3ef24b16ad5fd543484db80f78f0f64
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-03-30 18:03:38 +0200

    allow setting frame resources without an image

--- src/components/page/hiveEdit/frame/drawingCanvas/index.tsx
+++ src/components/page/hiveEdit/frame/drawingCanvas/index.tsx
@@ -658,7 +658,7 @@ export default function DrawingCanvas({
 							</span>
 						</Button>}
 
-					{showCells && frameSideFile.isCellsDetectionComplete && frameMetrics}
+					{showCells && frameMetrics}
 				</div>
 
 				<div style="flex-grow:1"></div>

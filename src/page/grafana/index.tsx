776cd976a41608bca1c3adef7e2daeb9dca1f263
Artjom Kurapov <artkurapov@gmail.com>
2024-08-18 21:06:14 +0300

    improve grafana page

--- src/page/grafana/index.tsx
+++ src/page/grafana/index.tsx
@@ -1,9 +1,20 @@
-import { grafanaUri } from '../../uri.ts'
-import React from 'react'
+import {useLiveQuery} from "dexie-react-hooks";
+
+import { grafanaUri } from '@/uri'
+import {getUser} from "@/models/user";
+import T from "@/shared/translate";
+import MessageSuccess from "@/shared/messageSuccess";
 
 
 export default function Grafana() {
+	let userStored = useLiveQuery(() =>  getUser(), [], null)
+
+	if(!userStored) return null
+
 	return (
-		<><iframe width="100%" height="100%" style={{border:0}} src={`${grafanaUri()}dashboards?orgId=1`} /></>
+		<>
+			{userStored.billingPlan !=='pro' && <MessageSuccess title={<T>You need to have a pro plan to access analytics</T>} isWarning={true} />}
+			<iframe width="100%" height="100%" style={{border:0}} src={`${grafanaUri()}dashboards?orgId=1`} />
+		</>
 	)
 }

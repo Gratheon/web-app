e5bf5e4fd4edb6c7832cd0659073cafb78f6ea9b
Artjom Kurapov <artkurapov@gmail.com>
2024-11-30 09:50:20 +0200

    andmetorm - add temperature graph for Estonia

--- src/shared/weather/index.tsx
+++ src/shared/weather/index.tsx
@@ -5,6 +5,8 @@ import Loading from '../loader'
 import Forecast from './forecast'
 import ErrorMsg from '../messageError'
 import {isEstonia} from "@/page/apiaryEdit";
+import {CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis} from "recharts";
+import {format} from "date-fns";
 
 const WEATHER_QUERY = gql`
 	query weather($lat: String!, $lng: String!) {
@@ -39,19 +41,54 @@ export default function Weather({ lat, lng }: { lat: any; lng: any }) {
 			return <ErrorMsg error={error} />
 		}
 
+		let formattedData = []
+
+		data.weatherEstonia.days.map((day: any, i: number) => {
+			console.log(day)
+			formattedData.push({
+				name: day,
+				temperature: data.weatherEstonia.temp[i],
+			})
+		})
 		console.log(data.weatherEstonia)
 		return (
 			<div style={{ padding: '0 10px' }}>
 				<h1>{data.weatherEstonia.closestLocation}</h1>
 
-				{data.weatherEstonia.data.map((day: any) => (
-					<>
-						<h2>{day.date}</h2>
-						<div>{day.day.phenomenon}</div>
-						<div>Temp: {day.day.temp.min}</div>
-						<div>Wind: {day.day.wind.min}</div>
-					</>
-				))}
+				{/*{data.weatherEstonia.data.map((day: any) => (*/}
+				{/*	<>*/}
+				{/*		<h2>{day.date}</h2>*/}
+				{/*		<div>{day.day.phenomenon}</div>*/}
+				{/*		<div>Temp: {day.day.temp.min}</div>*/}
+				{/*		<div>Wind: {day.day.wind.min}</div>*/}
+				{/*	</>*/}
+				{/*))}*/}
+
+
+				<LineChart
+					width={400}
+					height={200}
+					data={formattedData}
+					margin={{
+						top: 5,
+						right: 10,
+						left: 10,
+						bottom: 5,
+					}}
+				>
+					<CartesianGrid strokeDasharray="3 3"/>
+					<XAxis dataKey="name"/>
+					<YAxis/>
+					<Tooltip/>
+					{/*<Legend/>*/}
+					<Line
+						type="monotone"
+						dataKey="temperature"
+						stroke="#EEAAAA"
+						activeDot={{r: 2}}
+					/>
+				</LineChart>
+
 			</div>
 		)
 	}

148f99163e0f02bf9df3c888fe02c99bc003290b
Artjom Kurapov <artkurapov@gmail.com>
2024-11-30 09:58:54 +0200

    andmetorm - add wind graph for Estonia

--- src/shared/weather/index.tsx
+++ src/shared/weather/index.tsx
@@ -1,12 +1,9 @@
-import React from 'react'
-
-import { gql, useQuery } from '../../api'
+import {gql, useQuery} from '../../api'
 import Loading from '../loader'
 import Forecast from './forecast'
 import ErrorMsg from '../messageError'
 import {isEstonia} from "@/page/apiaryEdit";
 import {CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis} from "recharts";
-import {format} from "date-fns";
 
 const WEATHER_QUERY = gql`
 	query weather($lat: String!, $lng: String!) {
@@ -20,97 +17,120 @@ const WEATHER_ESTONIA_QUERY = gql`
 	}
 `
 
-export default function Weather({ lat, lng }: { lat: any; lng: any }) {
-	if(!lat) return
-	if(!lng) return
+export default function Weather({lat, lng}: { lat: any; lng: any }) {
+    if (!lat) return
+    if (!lng) return
 
-	if (isEstonia(lat, lng)) {
-		let { loading, error, data } = useQuery(WEATHER_ESTONIA_QUERY, {
-			variables: { lat: `${lat}`, lng: `${lng}` },
-		})
+    if (isEstonia(lat, lng)) {
+        let {loading, error, data} = useQuery(WEATHER_ESTONIA_QUERY, {
+            variables: {lat: `${lat}`, lng: `${lng}`},
+        })
 
-		if (loading) {
-			return <Loading />
-		}
+        if (loading) {
+            return <Loading/>
+        }
 
-		if (!data?.weatherEstonia) {
-			return <ErrorMsg error={'could not load weather'} />
-		}
+        if (!data?.weatherEstonia) {
+            return <ErrorMsg error={'could not load weather'}/>
+        }
 
-		if (error) {
-			return <ErrorMsg error={error} />
-		}
+        if (error) {
+            return <ErrorMsg error={error}/>
+        }
 
-		let formattedData = []
+        let formattedTempData = []
+        let formattedWindData = []
 
-		data.weatherEstonia.days.map((day: any, i: number) => {
-			console.log(day)
-			formattedData.push({
-				name: day,
-				temperature: data.weatherEstonia.temp[i],
-			})
-		})
-		console.log(data.weatherEstonia)
-		return (
-			<div style={{ padding: '0 10px' }}>
-				<h1>{data.weatherEstonia.closestLocation}</h1>
+        data.weatherEstonia.days.map((day: any, i: number) => {
+            console.log(day)
+            formattedTempData.push({
+                name: day,
+                temperature: data.weatherEstonia.temp[i],
+            })
+            formattedWindData.push({
+                name: day,
+                wind: data.weatherEstonia.wind[i],
+            })
+        })
+        console.log(data.weatherEstonia)
+        return (
+            <div style={{padding: '10px 20px', display:'flex'}}>
+                <h1>{data.weatherEstonia.closestLocation}</h1>
 
-				{/*{data.weatherEstonia.data.map((day: any) => (*/}
-				{/*	<>*/}
-				{/*		<h2>{day.date}</h2>*/}
-				{/*		<div>{day.day.phenomenon}</div>*/}
-				{/*		<div>Temp: {day.day.temp.min}</div>*/}
-				{/*		<div>Wind: {day.day.wind.min}</div>*/}
-				{/*	</>*/}
-				{/*))}*/}
+                <div>
+                    <h1>Temperature</h1>
+                    <LineChart
+                        width={400}
+                        height={200}
+                        data={formattedTempData}
+                        margin={{
+                            top: 5,
+                            right: 10,
+                            left: 10,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3"/>
+                        <XAxis dataKey="name"/>
+                        <YAxis/>
+                        <Tooltip/>
+                        <Line
+                            type="monotone"
+                            dataKey="temperature"
+                            stroke="#EEAAAA"
+                            activeDot={{r: 2}}
+                        />
+                    </LineChart>
+                </div>
 
+                <div>
+                    <h1>Wind speed</h1>
+                    <LineChart
+                        width={400}
+                        height={200}
+                        data={formattedWindData}
+                        margin={{
+                            top: 5,
+                            right: 10,
+                            left: 10,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3"/>
+                        <XAxis dataKey="name"/>
+                        <YAxis/>
+                        <Tooltip/>
+                        <Line
+                            type="monotone"
+                            dataKey="temperature"
+                            stroke="#EEAAAA"
+                            activeDot={{r: 2}}
+                        />
+                    </LineChart>
+                </div>
+            </div>
+        )
+    }
 
-				<LineChart
-					width={400}
-					height={200}
-					data={formattedData}
-					margin={{
-						top: 5,
-						right: 10,
-						left: 10,
-						bottom: 5,
-					}}
-				>
-					<CartesianGrid strokeDasharray="3 3"/>
-					<XAxis dataKey="name"/>
-					<YAxis/>
-					<Tooltip/>
-					{/*<Legend/>*/}
-					<Line
-						type="monotone"
-						dataKey="temperature"
-						stroke="#EEAAAA"
-						activeDot={{r: 2}}
-					/>
-				</LineChart>
+    let {loading, error, data} = useQuery(WEATHER_QUERY, {
+        variables: {lat: `${lat}`, lng: `${lng}`},
+    })
 
-			</div>
-		)
-	}
+    if (loading) {
+        return <Loading/>
+    }
 
-	let { loading, error, data } = useQuery(WEATHER_QUERY, {
-		variables: { lat: `${lat}`, lng: `${lng}` },
-	})
+    if (!data?.weather) {
+        return <ErrorMsg error={'could not load weather'}/>
+    }
 
-	if (loading) {
-		return <Loading />
-	}
+    if (error) {
+        return <ErrorMsg error={error}/>
+    }
 
-	if (!data?.weather) {
-		return <ErrorMsg error={'could not load weather'} />
-	}
-
-	if (error) {
-		return <ErrorMsg error={error} />
-	}
-
-	return (
-		<div style={{ padding: '0 10px' }}>
-			<Forecast data={data}/>
-		</div>
-	)}
+    return (
+        <div style={{padding: '0 10px'}}>
+            <Forecast data={data}/>
+        </div>
+    )
+}

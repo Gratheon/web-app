ae4b352b429bcea9d858b2865254c9545b9ba702
Artjom Kurapov <artkurapov@gmail.com>
2025-05-07 17:55:11 +0300

    ...

--- src/shared/input/index.tsx
+++ src/shared/input/index.tsx
@@ -1,10 +1,11 @@
-import { h } from 'preact';
-import { JSX } from 'preact';
+import { h, JSX } from 'preact';
 import styles from './styles.module.less';
 
-interface InputProps extends Omit<JSX.HTMLAttributes<HTMLInputElement>, 'label'> {
+interface InputProps extends JSX.HTMLAttributes<HTMLInputElement> {
   label?: JSX.Element | string;
   type?: string;
+  value?: string | number;
+  onChange?: (event: JSX.TargetedEvent<HTMLInputElement, Event>) => void;
 }
 
 export default function Input({ label, ...rest }: InputProps) {

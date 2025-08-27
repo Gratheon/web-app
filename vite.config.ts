413245d1eba1a3f4bae7b60f7a4edd96123abcbf
Artjom Kurapov <artkurapov@gmail.com>
2025-08-27 16:03:59 +0300

    update icons

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,16 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
+            src: '/icon_16x16.png',
+            sizes: '16x16',
+            type: 'image/png',
+          },
+          {
+            src: '/icon_32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+          },
+          {
             src: '/icon_96x96.png',
             sizes: '96x96',
             type: 'image/png',

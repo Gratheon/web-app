86954c40938ee0bbb420c7550ed41aa1129e3b24
Artjom Kurapov <artkurapov@gmail.com>
2025-11-22 15:18:48 +0200

    fix deploy

--- _setup-env.sh
+++ _setup-env.sh
@@ -1,9 +1,12 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
+export TMPDIR="$HOME/tmp"
+mkdir -p "$TMPDIR"
+
 nvm use
 export PNPM_HOME="$HOME/.local/share/pnpm"
 
 if ! command -v pnpm &> /dev/null; then
-  TMPDIR="$HOME/tmp" curl -fsSL https://get.pnpm.io/install.sh | sh -
+  curl -fsSL https://get.pnpm.io/install.sh | sh -
 fi

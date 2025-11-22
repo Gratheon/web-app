b3ae2dbc0e4865d306eba9b2f737886e99768bff
Artjom Kurapov <artkurapov@gmail.com>
2025-11-22 15:24:20 +0200

    fix deploy

--- _setup-env.sh
+++ _setup-env.sh
@@ -10,3 +10,8 @@ export PNPM_HOME="$HOME/.local/share/pnpm"
 if ! command -v pnpm &> /dev/null; then
   curl -fsSL https://get.pnpm.io/install.sh | sh -
 fi
+
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac

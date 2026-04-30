0dbf1d38429bcbc747536200b2fe1836c838c8c2
Artjom Kurapov <artkurapov@gmail.com>
2026-05-01 00:18:55 +0300

    ,,,

--- _build.sh
+++ _build.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env sh
 
+set -eu
+
 # Load nvm to make pnpm available
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
+rm -rf dist
 pnpm install
 mkdir -p ./node_modules/.tmp
 TMPDIR=./node_modules/.tmp pnpm build
 pnpm build-clean
 pnpm build-move
+
+test -f public/index.html
+test -f public/sw.js
 
 # includes robots.txt for bots
 # includes .well-known for google play app
